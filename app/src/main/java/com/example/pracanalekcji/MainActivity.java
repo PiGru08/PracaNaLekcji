9bef83b05e51815afeee9091c06bfd6a32084b91
PiGru08 <grucar77@gmail.com>
2024-11-20 13:39:16 +0100

    Dziaa wszystko

--- app/src/main/java/com/example/pracanalekcji/MainActivity.java
+++ app/src/main/java/com/example/pracanalekcji/MainActivity.java
@@ -24,13 +24,16 @@ public class MainActivity extends AppCompatActivity {
     TextView textViewWynikLosowania;
     TextView textViewWynikgry;
     Random liczba = new Random();
+    ArrayList<Integer> indeksy = new ArrayList<>();
+    int wynik;
+    int wynikGry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //obraz1 = findViewById(R.id.imageView5);
+
         //obraz1.setImageResource(R.drawable.kosc1);
-        int[] kosci = new int[]{R.drawable.kosc1,R.drawable.kosc2, R.drawable.kosc3, R.drawable.kosc4, R.drawable.kosc5, R.drawable.kosc6};
+        int[] kosci = new int[]{0,R.drawable.kosc1,R.drawable.kosc2, R.drawable.kosc3, R.drawable.kosc4, R.drawable.kosc5, R.drawable.kosc6};
         buttonResetWyniku = findViewById(R.id.buttonResetWyniku);
         buttonRzucKoscmi = findViewById(R.id.buttonRzuc);
         imageView1 = findViewById(R.id.imageView1);
@@ -50,16 +53,52 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        imageView1.setImageResource(kosci[liczba.nextInt(kosci.length)]);
-                        imageView2.setImageResource(kosci[liczba.nextInt(kosci.length)]);
-                        imageView3.setImageResource(kosci[liczba.nextInt(kosci.length)]);
-                        imageView4.setImageResource(kosci[liczba.nextInt(kosci.length)]);
-                        imageView5.setImageResource(kosci[liczba.nextInt(kosci.length)]);
-                        if(kosci == 1){
+                        wynik = 0;
+                        int los1 = liczba.nextInt(6)+1;
+                        int los2 = liczba.nextInt(6)+1;
+                        int los3 = liczba.nextInt(6)+1;
+                        int los4 = liczba.nextInt(6)+1;
+                        int los5 = liczba.nextInt(6)+1;
+                        imageView1.setImageResource(kosci[los1]);
+                        indeksy.add(los1);
+                        imageView2.setImageResource(kosci[los2]);
+                        indeksy.add(los2);
+                        imageView3.setImageResource(kosci[los3]);
+                        indeksy.add(los3);
+                        imageView4.setImageResource(kosci[los4]);
+                        indeksy.add(los4);
+                        imageView5.setImageResource(kosci[los5]);
+                        indeksy.add(los5);
+                       // for (int i = 0; i < indeksy.size(); i++) {
+                        //    if(los1 || los2 || los3 || los4 || los5 == indeksy.get(i)){
 
-                        }
+                       //     }
+                       // }
+                       wynik = los1 + los2 + los3 + los4 + los5;
+
+                        wynikGry = wynikGry + wynik;
+                        textViewWynikLosowania.setText("Wynik tego losowania: " + wynik);
+                        textViewWynikgry.setText("Wynik gry: "+wynikGry);
+
+                    }
+                }
+        );
+        buttonResetWyniku.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        wynik = 0;
+                        wynikGry = 0;
+                        textViewWynikLosowania.setText("Wynik tego losowania: " + wynik);
+                        textViewWynikgry.setText("Wynik gry: "+wynikGry);
+                        imageView1.setImageResource(R.drawable.kosc);
+                        imageView2.setImageResource(R.drawable.kosc);
+                        imageView3.setImageResource(R.drawable.kosc);
+                        imageView4.setImageResource(R.drawable.kosc);
+                        imageView5.setImageResource(R.drawable.kosc);
                     }
                 }
         );
     }
 }
+//TODO: Do zrobienia podliczanie przy tych samych kościach
